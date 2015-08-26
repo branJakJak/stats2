2de8bc6a93c4c667683a604da42e5e6c1ba670ce
kebyang <kelvo_link@yahoo.com>
2015-08-26 23:55:09 +0800

    updated size to 80

--- themes/baseAdmin3.0/views/site/index.php
+++ themes/baseAdmin3.0/views/site/index.php
@@ -212,7 +212,7 @@ Yii::app()->clientScript->registerScript('refreshContents', $refreshContents, CC
               
               <div class="widget-content" style="background-color: #000090;color: white;">
                 <div class='big-label'>
-                  <b id="managerMessagesContainer" style="font-size: 60px;">
+                  <b id="managerMessagesContainer" style="font-size: 80px;">
                     <?php echo $tbc ?>
                   </b>
                 </div>
