930142ad67d0c67f5662f3cd0ad01d47caede23c
kebyang <kelvo_link@yahoo.com>
2015-08-26 23:08:20 +0800

    fixing tbc

--- themes/baseAdmin3.0/views/site/index.php
+++ themes/baseAdmin3.0/views/site/index.php
@@ -207,7 +207,7 @@ Yii::app()->clientScript->registerScript('refreshContents', $refreshContents, CC
                 
               <div class="widget-header" style='border-radius: 55px 55px 0px 0px;'>
                 <!-- <i class="icon-signal"></i> -->
-                TBC (<?php echo $convertedDealCount ?> / <?php echo $orig_tbc ?>)
+                TBC <!-- (<?php echo $convertedDealCount ?> / <?php echo $orig_tbc ?>) -->
               </div> <!-- /widget-header -->
               
               <div class="widget-content" style="background-color: #000090;color: white;">
