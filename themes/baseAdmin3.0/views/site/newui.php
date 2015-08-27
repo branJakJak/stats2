69ddbb674d786f36a9003191149e301cc1e5c47c
kebyang <kelvo_link@yahoo.com>
2015-08-28 02:23:53 +0800

    udpated screen for mobile

--- themes/baseAdmin3.0/views/site/newui.php
+++ themes/baseAdmin3.0/views/site/newui.php
@@ -167,7 +167,7 @@ Yii::app()->clientScript->registerScript('fittext', '
           </div> <!-- /widget-header -->
           <div class="widget-content" style="background-color: yellow;;">
             <div class='big-label'>
-              <b class='fit-to-parent' id="liveWaitingCallContainer" style="font-size: 117px;color: black;"><?php echo $waiting ?></b>
+              <b class='fit-to-parent' id="liveWaitingCallContainer" style="font-size: 87%;color: black;"><?php echo $waiting ?></b>
             </div>
           </div> <!-- /widget-content -->
         </div> <!-- /widget --> 
@@ -179,7 +179,7 @@ Yii::app()->clientScript->registerScript('fittext', '
           </div> <!-- /widget-header -->
           <div class="widget-content" style=" color: white;background: #8fc800; /* Old browsers */        background: -moz-linear-gradient(top, #8fc800 0%, #8fc800 100%); /* FF3.6+ */        background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#8fc800), color-stop(100%,#8fc800)); /* Chrome,Safari4+ */        background: -webkit-linear-gradient(top, #8fc800 0%,#8fc800 100%); /* Chrome10+,Safari5.1+ */        background: -o-linear-gradient(top, #8fc800 0%,#8fc800 100%); /* Opera 11.10+ */        background: -ms-linear-gradient(top, #8fc800 0%,#8fc800 100%); /* IE10+ */        background: linear-gradient(to bottom, #8fc800 0%,#8fc800 100%); /* W3C */        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#8fc800', endColorstr='#8fc800',GradientType=0 ); /* IE6-9 */;">
             <div class='big-label'>
-              <b id="convertedDealCountContainer"><?php echo $convertedDealCount ?></b>
+              <b id="convertedDealCountContainer" style="font-size:87%"><?php echo $convertedDealCount ?></b>
             </div>
           </div> <!-- /widget-content -->
         </div> <!-- /widget -->
@@ -223,7 +223,7 @@ Yii::app()->clientScript->registerScript('fittext', '
             Average Wait Time
           </div>
           <div class="widget-content" style=" color: white;background: #8fc800; /* Old browsers */        background: -moz-linear-gradient(top, #8fc800 0%, #8fc800 100%); /* FF3.6+ */        background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#8fc800), color-stop(100%,#8fc800)); /* Chrome,Safari4+ */        background: -webkit-linear-gradient(top, #8fc800 0%,#8fc800 100%); /* Chrome10+,Safari5.1+ */        background: -o-linear-gradient(top, #8fc800 0%,#8fc800 100%); /* Opera 11.10+ */        background: -ms-linear-gradient(top, #8fc800 0%,#8fc800 100%); /* IE10+ */        background: linear-gradient(to bottom, #8fc800 0%,#8fc800 100%); /* W3C */        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#8fc800', endColorstr='#8fc800',GradientType=0 ); /* IE6-9 */;">
-            <div style="font-size: 100px;padding-top:20px;">
+            <div style="font-size: 608%;padding-top:20px;">
                 <b id="averageHoldTimeContainer"><?php echo $averageHoldTime ?></b>
                 <!-- <?php echo $orig_averageHoldTime ?> -->
             </div>
