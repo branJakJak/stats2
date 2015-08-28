c68b25ad9fb25dc64222a175ffc800a670fd6264
kebyang <kelvo_link@yahoo.com>
2015-08-28 17:11:37 +0800

    asasd

--- themes/baseAdmin3.0/views/site/index.php
+++ themes/baseAdmin3.0/views/site/index.php
@@ -35,7 +35,7 @@ $refreshContents = <<<EOL
     }
     setInterval(function () {
         refreshContents();
-    }, 10 * 1000);
+    }, ( ( 5 * 60 ) * 1000 );
 
 EOL;
 Yii::app()->clientScript->registerScript('refreshContents', $refreshContents, CClientScript::POS_READY);
