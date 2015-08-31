0c7fd252a6c22203d16d07e957723b7d9c55ee06
kebyang <kelvo_link@yahoo.com>
2015-08-31 17:22:15 +0800

    asdasd

--- protected/components/AverageContactRate.php
+++ protected/components/AverageContactRate.php
@@ -2,12 +2,17 @@
 class AverageContactRate {
     public static function getAverage()
     {
+    	$tempAveWaitTimeContainer = 0;
         $curlURL = "http://213.171.204.244/contactRate.php";
         $curlres = curl_init($curlURL);
         curl_setopt($curlres, CURLOPT_RETURNTRANSFER, true);
         $curlResRaw = curl_exec($curlres);
         $tempArr = json_decode($curlResRaw, true);
-        return $tempArr["avgWaitTime"];
+        $tempAveWaitTimeContainer = $tempArr["avgWaitTime"];
+        if (is_null($tempArr["avgWaitTime"]) == false) {
+        	$tempAveWaitTimeContainer = $tempArr["avgWaitTime"]
+        }
+        return $tempAveWaitTimeContainer;
     }
 
 }
