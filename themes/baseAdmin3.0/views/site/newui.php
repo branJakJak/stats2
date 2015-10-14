4818bb733007247eac6dd636f0a8d3edf1e56e2b
kebyang <kelvo_link@yahoo.com>
2015-10-14 21:27:04 +0800

    updated background color

--- themes/baseAdmin3.0/views/site/newui.php
+++ themes/baseAdmin3.0/views/site/newui.php
@@ -286,7 +286,7 @@ Yii::app()->clientScript->registerScript('refreshContents', $refreshContents, CC
           <div class="widget-header" style='border-radius: 55px 55px 0px 0px;'>
             REV D
           </div> <!-- /widget-header -->
-          <div class="widget-content" style=" color: white;background: #8fc800; /* Old browsers */        background: -moz-linear-gradient(top, #8fc800 0%, #8fc800 100%); /* FF3.6+ */        background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#8fc800), color-stop(100%,#8fc800)); /* Chrome,Safari4+ */        background: -webkit-linear-gradient(top, #8fc800 0%,#8fc800 100%); /* Chrome10+,Safari5.1+ */        background: -o-linear-gradient(top, #8fc800 0%,#8fc800 100%); /* Opera 11.10+ */        background: -ms-linear-gradient(top, #8fc800 0%,#8fc800 100%); /* IE10+ */        background: linear-gradient(to bottom, #8fc800 0%,#8fc800 100%); /* W3C */        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#8fc800', endColorstr='#8fc800',GradientType=0 ); /* IE6-9 */;">
+          <div class="widget-content" style="color: white;background: darkblue;">
             <div class='big-label'>
               <b id="liveRevDvalue" style="font-size:87%">
                 <?php echo $revDVal ?>
