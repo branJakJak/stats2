13bd938fcb767a5025334c942a709dfba92a791c
kebyang <kelvo_link@yahoo.com>
2015-10-14 22:49:16 +0800

    removed decimal

--- protected/components/LiveRevP.php
+++ protected/components/LiveRevP.php
@@ -10,6 +10,6 @@ class LiveRevP
 		$dbConnection = Yii::app()->roadtoriches;
 		$result = $dbConnection->createCommand("SELECT * FROM roadto_rich.display_pi_today")->queryRow();
 		$doubleEval = doubleval($result['Day_Total']);
-		return number_format($doubleEval,2);
+		return number_format($doubleEval);
 	}
 }
