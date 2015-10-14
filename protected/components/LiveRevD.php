938dbc93cdb9532f18a08f6ef52a1aa6c8f37a87
kebyang <kelvo_link@yahoo.com>
2015-10-14 22:14:24 +0800

    added updates

--- protected/components/LiveRevD.php
+++ protected/components/LiveRevD.php
@@ -10,7 +10,7 @@ class LiveRevD
 	public static function getValue()
 	{
 		$dbConnection = Yii::app()->roadtoriches;
-		$result = $dbConnection->createCommand("SELECT * FROM roadto_rich.debt_apporved_today")->queryRow();
+		$result = $dbConnection->createCommand("SELECT * FROM roadto_rich.display_pi_today")->queryRow();
 		return doubleval($result['total']);
 	}
 }
