de5b24729b1252d838eb2b42de987ff0a0afea7c
kebyang <kelvo_link@yahoo.com>
2015-11-20 01:22:13 +0800

    updated some fields

--- protected/components/LiveRevD.php
+++ protected/components/LiveRevD.php
@@ -9,15 +9,15 @@ class LiveRevD
 	
 	public static function getValue()
 	{
-		// $dbConnection = Yii::app()->roadtoriches;
-		// $result = $dbConnection->createCommand("SELECT * FROM roadto_rich.debt_apporved_today")->queryRow();
-		// $doubleEval = doubleval($result['total']);
-		// return round($doubleEval);
-		$curlURL = "http://213.171.204.244/pba.php";
-		$curlres = curl_init($curlURL);
-		curl_setopt($curlres, CURLOPT_RETURNTRANSFER, true);
-		$curlResRaw = curl_exec($curlres);
-		$curlResRaw = json_decode($curlResRaw,true);
-		return doubleval($curlResRaw["pba_sales"]);
+		$dbConnection = Yii::app()->roadtoriches;
+		$result = $dbConnection->createCommand("SELECT * FROM roadto_rich.pba_leads_today")->queryRow();
+		$doubleEval = doubleval($result['leads']);
+		return round($doubleEval);
+		// $curlURL = "http://213.171.204.244/pba.php";
+		// $curlres = curl_init($curlURL);
+		// curl_setopt($curlres, CURLOPT_RETURNTRANSFER, true);
+		// $curlResRaw = curl_exec($curlres);
+		// $curlResRaw = json_decode($curlResRaw,true);
+		// return doubleval($curlResRaw["pba_sales"]);
 	}
 }
