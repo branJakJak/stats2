e393b3cc1ae89da037d3ae586a28659818556ea7
Your Name <you@example.com>
2016-03-10 18:54:03 +0800

    fix

--- themes/baseAdmin3.0/views/site/newui.php
+++ themes/baseAdmin3.0/views/site/newui.php
@@ -217,7 +217,7 @@ Yii::app()->clientScript->registerScript('refreshContents', $refreshContents, CC
           </div> <!-- /widget-header -->
           <div class="widget-content" style=" color: white;background: #8fc800; /* Old browsers */        background: -moz-linear-gradient(top, #8fc800 0%, #8fc800 100%); /* FF3.6+ */        background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#8fc800), color-stop(100%,#8fc800)); /* Chrome,Safari4+ */        background: -webkit-linear-gradient(top, #8fc800 0%,#8fc800 100%); /* Chrome10+,Safari5.1+ */        background: -o-linear-gradient(top, #8fc800 0%,#8fc800 100%); /* Opera 11.10+ */        background: -ms-linear-gradient(top, #8fc800 0%,#8fc800 100%); /* IE10+ */        background: linear-gradient(to bottom, #8fc800 0%,#8fc800 100%); /* W3C */        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#8fc800', endColorstr='#8fc800',GradientType=0 ); /* IE6-9 */;">
             <div class='big-label'>
-              <b id="convertedDealCountContainer" style="font-size:87%">
+              <b id="convertedDealCountContainer" style="font-size:49%">
                 <?php //echo $convertedDealCount ?>
 
                 80.00 %
@@ -235,7 +235,7 @@ Yii::app()->clientScript->registerScript('refreshContents', $refreshContents, CC
           <?php if ($liveD >=0 && $liveD <= 5.99): ?>
           <div class="widget-content" style="background-color: red;color:white !important">
             <div class='big-label'>
-              <b id="liveDContainer" style="font-size:87%">
+              <b id="liveDContainer" style="font-size:50%">
               <?php //echo $liveD ?>
               75.00 %
               </b>
