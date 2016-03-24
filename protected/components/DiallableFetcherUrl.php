438440be1c1c6fade2a4a7dc8938cc95efeefb37
Your Name <you@example.com>
2016-03-24 19:01:39 +0800

    returned back to

--- protected/components/DiallableFetcherUrl.php
+++ protected/components/DiallableFetcherUrl.php
@@ -7,20 +7,22 @@ class DiallableFetcherUrl extends DiallableFetcher
 {
 	public function getByCampaignId($campaign_id)
 	{
-		$tempContainerIdContainer = $campaign_id;
-		$dbConnection = Yii::app()->directDialler;
-		$sqlCommandStr = <<<EOL
-		SELECT `dialable_leads`,`campaign_id` FROM `asterisk`.`vicidial_campaign_stats`
-		where `campaign_id` = '$campaign_id'
-EOL;
-		$commandContainer = $dbConnection->createCommand($sqlCommandStr);
-		$commandContainer->execute();
-		$result = $commandContainer->fetchAll();
-		// $curlURL = "http://$serverIp/getCampaignId.php?campaign_id=$campaign_id";
-		// $curlres = curl_init($curlURL);
-		// curl_setopt($curlres, CURLOPT_RETURNTRANSFER, true);
-		// $curlResRaw = curl_exec($curlres);
-		return isset($result[0]['dialable_leads']) ? $result[0]['dialable_leads']:0;
+// 		$tempContainerIdContainer = $campaign_id;
+// 		$dbConnection = Yii::app()->directDialler;
+// 		$sqlCommandStr = <<<EOL
+// 		SELECT `dialable_leads`,`campaign_id` FROM `asterisk`.`vicidial_campaign_stats`
+// 		where `campaign_id` = '$campaign_id'
+// EOL;
+// 		$commandContainer = $dbConnection->createCommand($sqlCommandStr);
+// 		$commandContainer->execute();
+// 		$result = $commandContainer->fetchAll();
+// 		
+		$curlURL = "http://$serverIp/getCampaignId.php?campaign_id=$campaign_id";
+		$curlres = curl_init($curlURL);
+		curl_setopt($curlres, CURLOPT_RETURNTRANSFER, true);
+		$curlResRaw = curl_exec($curlres);
+		return isset($curlResRaw[0]['dialable_leads']) ? $curlResRaw[0]['dialable_leads']:0;
+		// return isset($result[0]['dialable_leads']) ? $result[0]['dialable_leads']:0;
 	}
 }
 
