698c10a3ac59aa7d6630b053b969b2b593dde004
Your Name <you@example.com>
2016-03-24 18:44:38 +0800

    dsa

--- protected/components/DiallableFetcherUrl.php
+++ protected/components/DiallableFetcherUrl.php
@@ -7,13 +7,19 @@ class DiallableFetcherUrl extends DiallableFetcher
 {
 	public function getByCampaignId($campaign_id)
 	{
-		//81.138.138.57
-		$serverIp = Yii::app()->params['VICI_SERVER_IP'];
-		$curlURL = "http://$serverIp/getCampaignId.php?campaign_id=$campaign_id";
-		$curlres = curl_init($curlURL);
-		curl_setopt($curlres, CURLOPT_RETURNTRANSFER, true);
-		$curlResRaw = curl_exec($curlres);
-		$curlResRawArr = json_decode($curlResRaw,true);
-		return isset($curlResRawArr[0]['dialable_leads']) ? $curlResRawArr[0]['dialable_leads']:0;
+		$dbConnection = Yii::app()->roadtoriches;
+		$sqlCommandStr = <<<EOL
+		SELECT `dialable_leads`,`campaign_id` FROM `asterisk`.`vicidial_campaign_stats`
+		where `campaign_id` = :campaignId 
+EOL;
+		$commandContainer = $dbConnection->createCommand($sqlCommandStr);
+		$commandContainer->bindParam(":campaignId ",$campaign_id,PDO::PARAM_STR);
+		$commandContainer->execute();
+		$result = $commandContainer->fetchAll();
+		// $curlURL = "http://$serverIp/getCampaignId.php?campaign_id=$campaign_id";
+		// $curlres = curl_init($curlURL);
+		// curl_setopt($curlres, CURLOPT_RETURNTRANSFER, true);
+		// $curlResRaw = curl_exec($curlres);
+		return isset($result[0]['dialable_leads']) ? $result[0]['dialable_leads']:0;
 	}
 }
