26346ef6cc2b424d70e0fd96e1411848a1383d49
Your Name <you@example.com>
2016-05-05 18:48:12 +0800

    reverted to pba

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -29,7 +29,7 @@ class SiteController extends Controller
 	{
 		$diallableFetcher = new DiallableFetcherUrl();
 		$liveAVal = $diallableFetcher->getByCampaignId("LIVEA");
-		$livePbaValue = $diallableFetcher->getByCampaignId("PBAuto");
+		$livePbaValue = $diallableFetcher->getByCampaignId("PBA");
         $revDValue = LiveRevD::getValue();
         $revPValue = LiveRevP::getValue();
         
