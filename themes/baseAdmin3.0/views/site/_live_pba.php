9daf16024e305f545627fba3a2e7a9147b7fc9f5
Your Name <you@example.com>
2016-07-14 02:18:51 +0800

    dsa

--- themes/baseAdmin3.0/views/site/_live_pba.php
+++ themes/baseAdmin3.0/views/site/_live_pba.php
@@ -4,7 +4,7 @@
 if ($livePbaValue >= 182) {
   $applyStyle ="color: black;background: #5FFB17; /* Old browsers */        background: -moz-linear-gradient(top, #8fc800 0%, #8fc800 100%); /* FF3.6+ */        background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#8fc800), color-stop(100%,#8fc800)); /* Chrome,Safari4+ */        background: -webkit-linear-gradient(top, #8fc800 0%,#8fc800 100%); /* Chrome10+,Safari5.1+ */        background: -o-linear-gradient(top, #8fc800 0%,#8fc800 100%); /* Opera 11.10+ */        background: -ms-linear-gradient(top, #8fc800 0%,#8fc800 100%); /* IE10+ */        background: linear-gradient(to bottom, #8fc800 0%,#8fc800 100%); /* W3C */        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#8fc800', endColorstr='#8fc800',GradientType=0 ); /* IE6-9 */;";
 }else{
-  $applyStyle ="color: white;background: #8FC800;";
+  $applyStyle ="color: white;background: red;";
 }
 
 ?>
